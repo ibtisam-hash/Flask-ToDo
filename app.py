0ee61c4d9fb85b7a1bbde7deadab953184ed6de2
Ibtisam Hashmi <119796937+ibtisam-hash@users.noreply.github.com>
2023-09-24 15:00:43 +0500

    Completed

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ class Todo(db.Model):
 @app.route('/')
 def index():
     # show all todos
-    todo_list = Todo.query.all()
-    print(todo_list)
+    todo_list = Todo.query.order_by(Todo.id).all()
+    #print(todo_list)
     return render_template('base.html', todo_list=todo_list)
 
 @app.route("/add", methods=["POST"])
@@ -27,9 +27,24 @@ def add():
         db.session.commit()
         return redirect(url_for("index"))
 
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    #update item
+        todo = Todo.query.filter_by(id=todo_id).first()
+        todo.complete = not todo.complete
+        db.session.commit()
+        return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    #delete item
+        todo = Todo.query.filter_by(id=todo_id).first()
+        db.session.delete(todo)
+        db.session.commit()
+        return redirect(url_for("index"))
+
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-
-
     app.run(debug=True)
